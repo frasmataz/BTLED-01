b009caf8461bf5a555442877f6bf4543e1886bb5
Fraser Sharp <fraserrsharp@gmail.com>
2021-04-01 04:15:02 +0100

    feat: Add HSV single colour selection in app

--- android/app/src/main/java/com/frasersharp/btled/MainActivity.kt
+++ android/app/src/main/java/com/frasersharp/btled/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.frasersharp.btled
 
 import android.graphics.Color
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.blue
+import androidx.core.graphics.green
+import androidx.core.graphics.red
 import kotlin.math.floor
 
 
@@ -28,11 +32,60 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         connectBluetooth()
+
+        val callback = object :
+            SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+                updateColourPreview()
+            }
+
+            override fun onStartTrackingTouch(seek: SeekBar) {
+
+                updateColourPreview()
+            }
+
+            override fun onStopTrackingTouch(seek: SeekBar) {
+                updateColourPreview()}
+        }
+        val hueBar = findViewById<View>(R.id.seekBarHue) as SeekBar
+        val satBar = findViewById<View>(R.id.seekBarSaturation) as SeekBar
+        val intBar = findViewById<View>(R.id.seekBarIntensity) as SeekBar
+
+        hueBar.setOnSeekBarChangeListener(callback)
+        satBar.setOnSeekBarChangeListener(callback)
+        intBar.setOnSeekBarChangeListener(callback)
     }
 
-    fun connectBluetooth() {
+    fun updateColourPreview() {
+        val hue = findViewById<SeekBar>(R.id.seekBarHue).progress / 10.0f
+        val saturation = findViewById<SeekBar>(R.id.seekBarSaturation).progress / 1000.0f
+        val intensity = findViewById<SeekBar>(R.id.seekBarIntensity).progress / 1000.0f
+
+        val colour = Color.HSVToColor(255, floatArrayOf(hue, saturation, intensity))
+        Log.w("BTLED", "h $hue s $saturation i $intensity")
+        Log.w("BTLED", "r ${colour.red} g ${colour.green} b ${colour.blue}")
+
+        val button = findViewById<Button>(R.id.setColourButton)
+        button.setBackgroundColor(colour)
+    }
+
+    fun sendSingleColour(view: View) {
+        val hue = findViewById<SeekBar>(R.id.seekBarHue).progress / 10.0f
+        val saturation = findViewById<SeekBar>(R.id.seekBarSaturation).progress / 1000.0f
+        val intensity = findViewById<SeekBar>(R.id.seekBarIntensity).progress / 1000.0f
+
+        val colour = Color.HSVToColor(floatArrayOf(hue, saturation, intensity))
+        sendColourList(
+            arrayOf(
+                AnimationStep(Color.valueOf(colour), 100)
+            )
+        )
+    }
+
+    private fun connectBluetooth() {
         bluetoothSerial =
-            BluetoothSerial(this.baseContext,
+            BluetoothSerial(
+                this.baseContext,
                 BluetoothSerial.MessageHandler { bufferSize, buffer -> doRead(bufferSize, buffer) },
                 "HC"
             )
@@ -87,61 +140,75 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun patternButtonOff(view: View) {
-        sendColourList(arrayOf(
-            AnimationStep(Color.valueOf(0.0f, 0.0f, 0.0f), 100)
-        ));
+        sendColourList(
+            arrayOf(
+                AnimationStep(Color.valueOf(0.0f, 0.0f, 0.0f), 100)
+            )
+        );
     }
 
     fun patternButtonColdWhite(view: View) {
-        sendColourList(arrayOf(
-            AnimationStep(Color.valueOf(1.0f, 1.0f, 1.0f), 100)
-        ));
+        sendColourList(
+            arrayOf(
+                AnimationStep(Color.valueOf(1.0f, 1.0f, 1.0f), 100)
+            )
+        );
     }
 
     fun patternButtonWarmWhite(view: View) {
-        sendColourList(arrayOf(
-            AnimationStep(Color.valueOf(1.0f, 0.35f, 0.05f), 100, 20, 3000)
-        ));
+        sendColourList(
+            arrayOf(
+                AnimationStep(Color.valueOf(1.0f, 0.35f, 0.05f), 100, 20, 3000)
+            )
+        );
     }
 
     fun patternButtonPurpleFade(view: View) {
-        sendColourList(arrayOf(
-            AnimationStep(Color.valueOf(1.0f, 0.0f, 1.0f), 100, 50, 50),
-            AnimationStep(Color.valueOf(0.3f, 0.0f, 1.0f), 100, 80, 50),
-            AnimationStep(Color.valueOf(1.0f, 0.0f, 0.0f), 100, 50, 50),
-            AnimationStep(Color.valueOf(0.6f, 0.0f, 1.0f), 100, 50, 50),
-            AnimationStep(Color.valueOf(1.0f, 0.0f, 0.6f), 100, 50, 50),
-            AnimationStep(Color.valueOf(0.2f, 0.0f, 1.0f), 100, 50, 50)
-        ));
+        sendColourList(
+            arrayOf(
+                AnimationStep(Color.valueOf(1.0f, 0.0f, 1.0f), 100, 50, 50),
+                AnimationStep(Color.valueOf(0.3f, 0.0f, 1.0f), 100, 80, 50),
+                AnimationStep(Color.valueOf(1.0f, 0.0f, 0.0f), 100, 50, 50),
+                AnimationStep(Color.valueOf(0.6f, 0.0f, 1.0f), 100, 50, 50),
+                AnimationStep(Color.valueOf(1.0f, 0.0f, 0.6f), 100, 50, 50),
+                AnimationStep(Color.valueOf(0.2f, 0.0f, 1.0f), 100, 50, 50)
+            )
+        );
     }
 
     fun patternButtonCyanFade(view: View) {
-        sendColourList(arrayOf(
-            AnimationStep(Color.valueOf(0.0f, 1.0f, 1.0f), 100),
-            AnimationStep(Color.valueOf(0.0f, 0.4f, 1.0f), 100),
-            AnimationStep(Color.valueOf(0.0f, 1.0f, 0.3f), 100),
-            AnimationStep(Color.valueOf(0.0f, 0.6f, 0.8f), 100),
-            AnimationStep(Color.valueOf(0.0f, 1.0f, 0.1f), 100),
-            AnimationStep(Color.valueOf(0.0f, 0.0f, 1.0f), 100)
-        ));
+        sendColourList(
+            arrayOf(
+                AnimationStep(Color.valueOf(0.0f, 1.0f, 1.0f), 100),
+                AnimationStep(Color.valueOf(0.0f, 0.4f, 1.0f), 100),
+                AnimationStep(Color.valueOf(0.0f, 1.0f, 0.3f), 100),
+                AnimationStep(Color.valueOf(0.0f, 0.6f, 0.8f), 100),
+                AnimationStep(Color.valueOf(0.0f, 1.0f, 0.1f), 100),
+                AnimationStep(Color.valueOf(0.0f, 0.0f, 1.0f), 100)
+            )
+        );
     }
 
     fun patternButtonCandle(view: View) {
-        sendColourList(arrayOf(
-            AnimationStep(Color.valueOf(0.5f, 0.1f, 0.0f), 100, 30, 100),
-            AnimationStep(Color.valueOf(0.7f, 0.2f, 0.0f), 100, 20, 100),
-            AnimationStep(Color.valueOf(0.3f, 0.05f, 0.0f), 100, 35, 100),
-            AnimationStep(Color.valueOf(0.5f, 0.1f, 0.0f), 100, 20, 100),
-            AnimationStep(Color.valueOf(0.6f, 0.2f, 0.0f), 100, 10, 100),
-            AnimationStep(Color.valueOf(0.5f, 0.1f, 0.0f), 100, 20, 100),
-            AnimationStep(Color.valueOf(0.7f, 0.2f, 0.0f), 100, 30, 100)
-        ));
+        sendColourList(
+            arrayOf(
+                AnimationStep(Color.valueOf(0.5f, 0.1f, 0.0f), 100, 30, 100),
+                AnimationStep(Color.valueOf(0.7f, 0.2f, 0.0f), 100, 20, 100),
+                AnimationStep(Color.valueOf(0.3f, 0.05f, 0.0f), 100, 35, 100),
+                AnimationStep(Color.valueOf(0.5f, 0.1f, 0.0f), 100, 20, 100),
+                AnimationStep(Color.valueOf(0.6f, 0.2f, 0.0f), 100, 10, 100),
+                AnimationStep(Color.valueOf(0.5f, 0.1f, 0.0f), 100, 20, 100),
+                AnimationStep(Color.valueOf(0.7f, 0.2f, 0.0f), 100, 30, 100)
+            )
+        );
     }
 
     fun patternButtonEvilPulse(view: View) {
-        sendColourList(arrayOf(
-            AnimationStep(Color.valueOf(1.0f, 0.0f, 0.0f), 1),
-            AnimationStep(Color.valueOf(0.0f, 0.0f, 0.0f), 1)
-        ));
+        sendColourList(
+            arrayOf(
+                AnimationStep(Color.valueOf(1.0f, 0.0f, 0.0f), 1),
+                AnimationStep(Color.valueOf(0.0f, 0.0f, 0.0f), 1)
+            )
+        );
     }
 }
